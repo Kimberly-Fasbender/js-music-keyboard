624ddbb4bcc471e4ad1a39175f7194722fc50679
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-05-28 16:31:48 -0700

    added functions for playing a note by click event or keydown event

--- noise.js
+++ noise.js
@@ -1,3 +1,38 @@
-$(document).ready( function() {
-  // your code here
+$(document).ready( () => {
+
+  const Element = (file) => {
+    return document.getElementById(file);
+  }
+
+  const notes = {
+    'c': Element("cAudio"),
+    'd': Element("dAudio"),
+    'e': Element("eAudio"),
+    'f': Element("fAudio"),
+    'g': Element("gAudio"),
+    'a': Element("aAudio"),
+    'b': Element("bAudio"),
+  }
+
+  $('.note').click(function(event) {
+    const key = `${this.innerHTML}`;
+      if (notes[key].paused) {
+        notes[key].play();
+      } else {
+        notes[key].currentTime = 0;
+      }
+  });
+
+  $('body').keydown(function(event) {
+    const key = String.fromCharCode(event.keyCode).toLowerCase();
+   
+      if (notes[key]) {
+        if (notes[key].paused) {
+          notes[key].play();
+        } else {
+          notes[key].currentTime = 0;
+        }
+      }
+  });
+
 });
